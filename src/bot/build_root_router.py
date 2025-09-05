e169f9a1fe257c96fddd4d4dd4ec2b3f4b914917
Олег <77684721+NeoTheBestDeveloper@users.noreply.github.com>
2025-09-05 17:13:30 +0300

    Update build_root_router.py

--- src/bot/build_root_router.py
+++ src/bot/build_root_router.py
@@ -17,12 +17,12 @@ def build_root_router() -> RootRouter:
     root_router = RootRouter(throttling=True)
 
     include_registration_router(root_router)
+    include_group_panel_router(root_router)
+    include_admin_router(root_router)
     include_help_command_router(root_router)
     include_poll_attendance_router(root_router)
     include_profile_router(root_router)
     include_show_schedule_router(root_router)
-    include_admin_router(root_router)
-    include_group_panel_router(root_router)
     include_show_attendance_router(root_router)
 
     return root_router
